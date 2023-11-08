65ced12ea98bd712b34445963ea972cf27f0084d
Mahardyxxa <149994578+Mahardyxxa@users.noreply.github.com>
2023-11-08 14:41:17 +0700

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tech</title>
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+            <div class="logo"><a href=''>Mahardyxa</a></div>
+            <div class="menu">
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#courses">Maintenance</a></li>
+                    <li><a href="#tutors">Component</a></li>
+                    <li><a href="#partners">Partners</a></li>
+                    <li><a href="../index.php" class="tbl-biru">Transaksi</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="../login.html" class="tbl-biru">Sign Up</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="wrapper">
+        <!-- untuk home -->
+        <section id="home">
+            <img src="13038.jpg" width="600"
+            height="350"/>
+            <div class="kolom">
+                <p class="deskripsi">Pengertian Teknologi</p>
+                <h2>Perkembangan Komponen Komputer</h2>
+                <p><a href="" class="tbl-pink">Tentang Komponen</a></p>
+            </div>
+        </section>
+
+        <!-- untuk courses -->
+        <section id="courses">
+            <div class="kolom">
+                <p class="deskripsi">Tentang Komponen</p>
+                <h2>Pengertian Komponen Komputer</h2>
+                <p>komponen merupakan bagian dari keseluruhan yang membentuk suatu kesatuan. Selain itu, KBBI juga menyebutkan bahwa komponen bisa diartikan juga sebagai unsur, teman-teman. Komponen juga memiliki arti perangkat elektronik fisik yang merupakan bagian dari rangkaian.</p>
+                <p><a href="" class="tbl-biru">Pelajari Lebih Lanjut</a></p>
+            </div>
+                <img src="5052628.jpg" width="500"/>
+        </section>
+
+        <!-- untuk tutors -->
+        <section id="tutors">
+            <div class="tengah">
+                <div class="kolom">
+                    <p class="deskripsi">Komponen Komputer</p>
+                    <h2>Berikut Ini Komponennya</h2>
+                </div>
+
+                <div class="tutor-list">
+                    <div class="kartu-tutor">
+                        <img src="Graphics-Card-PNG-Picture.png"/>
+                        <p>VGA Komputer</p>
+                    </div>
+                    <div class="kartu-tutor">
+                        <img src="CPU-PNG-Images.png"/>
+                        <p>Random Acces Memory</p>
+                    </div>
+                    <div class="kartu-tutor">
+                        <img src="pngwing.com.png"/>
+                        <p>Casing Komputer</p>
+                    </div>
+                    <div class="kartu-tutor">
+                        <img src="PngItem_6501292.png"/>
+                        <p>Monitor Komputer</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- untuk partners -->
+        <section id="partners">
+            <div class="tengah">
+                <div class="kolom">
+                    <p class="deskripsi">Our Top Brand</p>
+                    <h2>Brand</h2>
+                </div>
+
+                <div class="partner-list">
+                    <div class="kartu-partner">
+                        <img src="pngwing.com (1).png"/>
+                    </div>
+                    <div class="kartu-partner">
+                        <img src="pngwing.com (2).png"/>
+                    </div>
+                    <div class="kartu-partner">
+                        <img src="pngwing.com (3).png"/>
+                    </div>
+                    <div class="kartu-partner">
+                        <img src="pngwing.com (4).png"/>
+                    </div>
+                    <div class="kartu-partner">
+                        <img src="pngwing.com (5).png"/>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+    <div id="contact">
+        <div class="wrapper">
+            <div class="footer">
+                <div class="footer-section">
+                    <h3>Mahardyxa.</h3>
+                    <p>The Nigga Was Taken</p>
+                </div>
+                <div class="footer-section">
+                    <h3>About</h3>
+                    <p>Pengertian Komponen ini Untuk Adick" Yang Mau Belajar Komputer</p>
+                </div>
+                <div class="footer-section">
+                    <h3>Contact</h3>
+                    <p>Jl. Laksda Adisucipto Wringinanom Gresik</p>
+                    <p>Kode Pos: 57365</p>
+                </div>
+                <div class="footer-section">
+                    <h3>Social</h3>
+                    <p><b>YouTube: </b>Programming di Mahardyxa.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="copyright">
+        <div class="wrapper">
+            &copy; 2024. <b>Mahardyxa.</b> The Nigga Was Taken.
+        </div>
+    </div>
+</body>
+</html>
